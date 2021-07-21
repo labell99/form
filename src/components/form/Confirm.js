50aecc7ca3ef3b10f2950f44755c7e03114d7b92
lee abell <lee.abell@finclusionsystems.com>
2021-07-21 16:01:00 +0100

    update validation

--- src/components/form/Confirm.js
+++ src/components/form/Confirm.js
@@ -93,7 +93,7 @@ const Confirm = ({ values, submit, setConfirmShow, confirmShow, setSubmitting })
           onClick={submit}
           style={{
             flexGrow: 1,
-            background: #0073BF,
+            background: `linear-gradient(90deg, ${theme.palette.primary.main},${theme.palette.secondary.main})`,
           }}
         >
           Order now
