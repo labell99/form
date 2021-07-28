b11577059d655b2a068c1add4e00b714c5c3797c
lee abell <lee.abell@finclusionsystems.com>
2021-07-28 18:53:00 +0100

    update axiom

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-theme-material-ui`,
+    `gatsby-plugin-csp`,
     {
       resolve: `gatsby-plugin-material-ui`,
       options: {
@@ -17,6 +18,22 @@ module.exports = {
           injectFirst: true,
         },
       },
+    },
+    {
+      resolve: `gatsby-plugin-csp`,
+      options: {
+        disableOnDev: true,
+        reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
+        mergeScriptHashes: true, // you can disable scripts sha256 hashes
+        mergeStyleHashes: true, // you can disable styles sha256 hashes
+	        mergeDefaultDirectives: true,
+	        directives: {
+	          "script-src": "'self' www.google-analytics.com",
+	          "style-src": "'self' 'unsafe-inline'",
+	          "img-src": "'self' data: www.google-analytics.com"
+	          // you can add your directives or override defaults
+	        }
+      }
     },
     `gatsby-plugin-styled-components`,
     // `gatsby-plugin-offline`,
