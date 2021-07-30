1fa8164608985b8896f0eabef44740eb88d9e66e
lee abell <lee.abell@finclusionsystems.com>
2021-07-30 17:40:30 +0100

    first commit

--- src/components/form/OrderForm.js
+++ src/components/form/OrderForm.js
@@ -67,7 +67,7 @@ const OrderForm = () => {
       });
 
       //var authtoken = "Bearer " + data.jwt;
-      var authtoken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZjgwZTE5ZWU1NmJhMGY2MGU1YTViZSIsImlhdCI6MTYyNzU3MzI4OSwiZXhwIjoxNjMwMTY1Mjg5fQ.dHJ1veAig90JLUGqtcUKO02RV6hZbAYH-RM9A405sZ0";
+      var authtoken = "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZjgwZTE5ZWU1NmJhMGY2MGU1YTViZSIsImlhdCI6MTYyNzU3MzI4OSwiZXhwIjoxNjMwMTY1Mjg5fQ.dHJ1veAig90JLUGqtcUKO02RV6hZbAYH-RM9A405sZ0";
 
       console.log("jwt: ",authtoken);
 
