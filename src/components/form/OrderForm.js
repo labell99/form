999ef6f638cbe3e0df1e7316c44d7aa659bbfcdc
lee abell <lee.abell@finclusionsystems.com>
2021-08-02 15:55:17 +0100

    first commit

--- src/components/form/OrderForm.js
+++ src/components/form/OrderForm.js
@@ -153,7 +153,7 @@ const OrderForm = () => {
         >
           {formik => (
             <Form name="Orders" data-netlify="true">
-              <Card style={{ width: '20rem' }}>
+              <Card style={{ width: '40rem' }}>
                 <CardContent>
                   {/* Header */}
                   <OrderHeader />
